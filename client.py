b9dac8f1e83005fde9b49a1834c22699e9428ee5
cheezedoodles <hougvapes@gmail.com>
2022-07-19 19:47:42 +0300

    renamed buttons and moved some logic to separate functions

--- client.py
+++ client.py
@@ -6,22 +6,30 @@ import asyncio
 import threading
 import queue
 
-from PyQt5.QtWidgets import (QMainWindow, QAction, QApplication,
-                             QDesktopWidget, QGridLayout, QPushButton, QWidget,
-                             QLineEdit, QPlainTextEdit, QLabel,
-                             QMessageBox)
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QAction,
+    QApplication,
+    QDesktopWidget,
+    QGridLayout,
+    QPushButton,
+    QWidget,
+    QLineEdit,
+    QPlainTextEdit,
+    QLabel,
+    QMessageBox,
+)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject
 
-from vernamcipher import (generate_key, encrypt,
-                          decrypt, calculate_offset)
+from vernamcipher import generate_key, encrypt, decrypt, calculate_offset
 
 
-WEBSOCKET_URL = 'ws://127.0.0.1:8000/ws/chat/'
+WEBSOCKET_URL = "ws://127.0.0.1:8000/ws/chat/"
 
-BASE_URL = 'http://127.0.0.1:8000/'
+BASE_URL = "http://127.0.0.1:8000/"
 
-ORIGIN = 'http://127.0.0.1:8000'
+ORIGIN = "http://127.0.0.1:8000"
 
 
 class ChatThread(QObject, threading.Thread):
@@ -39,13 +47,12 @@ class ChatThread(QObject, threading.Thread):
 
     async def receiver(self):
         async with websockets.connect(
-            WEBSOCKET_URL + f'{self.chat_id}/',
-            origin=ORIGIN
+            WEBSOCKET_URL + f"{self.chat_id}/", origin=ORIGIN
         ) as websocket:
             self.ws = websocket
             while True:
                 self.new_msg = await self.ws.recv()
-                message_queue.put(json.loads(self.new_msg)['message'])
+                message_queue.put(json.loads(self.new_msg)["message"])
                 self.rcv.emit()
 
     def send_wrapper(self, msg):
@@ -67,13 +74,12 @@ class ChatThread(QObject, threading.Thread):
 
 
 class Menu(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.populateUI()
         self.resize(400, 400)
         self.center()
-        self.setWindowTitle('Menu')
+        self.setWindowTitle("Menu")
         self.show()
 
     def center(self):
@@ -92,19 +98,18 @@ class Menu(QMainWindow):
     def createMenu(self):
         menuBar = self.menuBar()
 
-        fileMenu = menuBar.addMenu('&File')
+        fileMenu = menuBar.addMenu("&File")
         fileMenu.addAction(self.createExitAction())
 
     def createExitAction(self):
-        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
+        exitAction = QAction(QIcon("exit.png"), "&Exit", self)
+        exitAction.setShortcut("Ctrl+Q")
+        exitAction.setStatusTip("Exit application")
         exitAction.triggered.connect(self.close)
         return exitAction
 
 
 class MenuLogin(QWidget):
-
     def __init__(self):
         super().__init__()
         self.username_label = QLabel('<font size="4"> username </font>')
@@ -116,9 +121,9 @@ class MenuLogin(QWidget):
         self.password = QLineEdit(self)
         self.password.setEchoMode(QLineEdit.Password)
 
-        self.login_button = QPushButton('Login', self)
-        self.register_button = QPushButton('Register', self)
-        self.auth_errors = QLabel('')
+        self.login_button = QPushButton("Login", self)
+        self.register_button = QPushButton("Register", self)
+        self.auth_errors = QLabel("")
 
         self.login_button.clicked.connect(self.login_user)
         self.register_button.clicked.connect(self.register_user)
@@ -142,56 +147,55 @@ class MenuLogin(QWidget):
 
     @pyqtSlot()
     def login_user(self):
-        self.auth_errors.setText('')
+        self.auth_errors.setText("")
         username = self.username.text()
         password = self.password.text()
         try:
             login_request = requests.post(
-                BASE_URL + 'api/login/',
-                json={'username': username,
-                      'password': password}
+                BASE_URL + "api/login/",
+                json={"username": username, "password": password},
             ).json()
-            self.token = login_request['token']
+            self.token = login_request["token"]
         except KeyError:  # TODO: Не обратывай блоком try нормальное поведение системы
-            self.auth_errors.setText('Invalid credentials')
+            self.auth_errors.setText("Invalid credentials")
             return
 
-        self.availableChatsWindow = AvailableChats(self.token, username)  # TODO: В __init__ или класс как None
+        self.availableChatsWindow = AvailableChats(self.token, username)
         self.availableChatsWindow.show()
+
+    def signup_msgbox(self):
+        msgbox = QMessageBox()
+        msgbox.setText("Signed up successfully")
+        msgbox.setWindowTitle("Registration")
+        msgbox.setStandardButtons(QMessageBox.Ok)
+        msgbox.exec_()
 
     @pyqtSlot()
     def register_user(self):
-        self.auth_errors.setText('')
+        self.auth_errors.setText("")
 
         username = self.username.text()
         password = self.password.text()
 
-        user_existance = requests.get(
-            BASE_URL + f'api/users/{username}/'
-        ).json()
+        user_existance = requests.get(BASE_URL + f"api/users/{username}/").json()
 
-        if 'detail' in user_existance:
+        if "detail" in user_existance:
             requests.post(
-                BASE_URL + 'api/create/',
-                json={'username': username,
-                      'password': password}
+                BASE_URL + "api/create/",
+                json={"username": username, "password": password},
             ).json()
-            msgbox = QMessageBox()  # TODO: Можно вынести в функцию
-            msgbox.setText('Signed up successfully')
-            msgbox.setWindowTitle('Registration')
-            msgbox.setStandardButtons(QMessageBox.Ok)
-            msgbox.exec_()
-
+            self.signup_msgbox()
         else:
-            self.auth_errors.setText('Account already exists')
+            self.auth_errors.setText("Account already exists")
 
 
 class AvailableChats(QWidget):
-
     def __init__(self, token, username):
         super().__init__()
         self.token = token
         self.current_user = username
+        self.chatWindow = None
+        self.createChat = None
 
         self.resize(400, 400)
         self.center()  # TODO: Довольно часто встречается в коде, может стоит вынести в Mix-in?
@@ -201,35 +205,37 @@ class AvailableChats(QWidget):
 
         self.pickChat = QLineEdit(self)
 
-        self.continueBtn = QPushButton('Continue', self)
-        self.createChatBtn = QPushButton('Create chat', self)
+        self.continue_button = QPushButton("Continue", self)
+        self.createchat_button = QPushButton("Create chat", self)
 
         chats = self.get_chats()
         self.ids = []
-        self.chats.appendPlainText('Pick the chat number and press continue')
+        self.chats.appendPlainText("Pick the chat number and press continue")
 
         for _, chat in enumerate(chats):
-            self.ids.append(chat['id'])
+            self.ids.append(chat["id"])
 
-            self.chats.appendPlainText(  # TODO: лучше используй f-строки
-                'Chat ' + str(chat['id']) + ' ' + '--' +
-                str(chat['sent_from_id']) + ' ' +
-                'with' + ' ' +
-                str(chat['sent_to_id'])
-            )
+            chat_id = str(chat["id"])
+            sent_from = str(chat["sent_from_id"])
+            sent_to = str(chat["sent_to_id"])
 
-        self.continueBtn.clicked.connect(self.pick_chat)
-        self.createChatBtn.clicked.connect(self.create_chat)
+            self.chats.appendPlainText(f"Chat {chat_id} -- {sent_from} with {sent_to}")
+
+        self.continue_button.clicked.connect(self.pick_chat)
+        self.createchat_button.clicked.connect(self.create_chat)
 
         self.show()
 
-        # TODO: вынеси gridы таки в функции
+        self.create_grid()
+
+    def create_grid(self):
         grid = QGridLayout()
+
         grid.setSpacing(3)
         grid.addWidget(self.chats, 0, 0, 1, 3)
         grid.addWidget(self.pickChat, 1, 0, 1, 1)
-        grid.addWidget(self.continueBtn, 1, 2)
-        grid.addWidget(self.createChatBtn, 2, 2)
+        grid.addWidget(self.continue_button, 1, 2)
+        grid.addWidget(self.createchat_button, 2, 2)
 
         self.setLayout(grid)
 
@@ -242,9 +248,8 @@ class AvailableChats(QWidget):
     @pyqtSlot()
     def get_chats(self):
         chats = requests.get(
-            BASE_URL + 'api/chats/',
-            headers={'Authorization': f'Token {self.token}'}
-        ).json()['results']
+            BASE_URL + "api/chats/", headers={"Authorization": f"Token {self.token}"}
+        ).json()["results"]
         return chats
 
     @pyqtSlot()
@@ -252,13 +257,12 @@ class AvailableChats(QWidget):
         chat_num = int(self.pickChat.text())
         if chat_num in self.ids:
 
-            self.chatWindow = Chat(chat_num, self.token)  # TODO: Для всего, что ты создаёшь в
-            # TODO: других функциях объявляй переменную в __init__
+            self.chatWindow = Chat(chat_num, self.token)
             self.chatWindow.show()
 
             self.hide()
             return
-        self.pickChat.setText('Invalid id')
+        self.pickChat.setText("Invalid id")
 
     @pyqtSlot()
     def create_chat(self):
@@ -269,11 +273,11 @@ class AvailableChats(QWidget):
 
 
 class CreateChat(QWidget):
-
     def __init__(self, token, username):
         super().__init__()
         self.token = token
         self.current_user = username
+        self.availableChats = None
 
         self.resize(400, 400)
         self.center()
@@ -283,17 +287,20 @@ class CreateChat(QWidget):
 
         self.pickUser = QLineEdit(self)
 
-        self.createChatBtn = QPushButton('Create chat', self)
-        self.createChatBtn.clicked.connect(self.create_chat)
+        self.createchat_button = QPushButton("Create chat", self)
+        self.createchat_button.clicked.connect(self.create_chat)
 
         self.users = self.show_users()
         self.show()
 
+        self.create_grid()
+
+    def create_grid(self):
         grid = QGridLayout()
         grid.setSpacing(3)
         grid.addWidget(self.chats, 0, 0, 1, 3)
         grid.addWidget(self.pickUser, 1, 0, 1, 1)
-        grid.addWidget(self.createChatBtn, 1, 2)
+        grid.addWidget(self.createchat_button, 1, 2)
 
         self.setLayout(grid)
 
@@ -307,16 +314,13 @@ class CreateChat(QWidget):
         users_to_check = []
 
         users = requests.get(
-            BASE_URL + 'api/users/',
-            headers={'Authorization': f'Token {self.token}'}
-        ).json()['results']
+            BASE_URL + "api/users/", headers={"Authorization": f"Token {self.token}"}
+        ).json()["results"]
 
-        self.chats.setPlainText('')
+        self.chats.setPlainText("")
         for _, user in enumerate(users):
-            self.chats.appendPlainText(
-                user['username']
-            )
-            users_to_check.append(user['username'])
+            self.chats.appendPlainText(user["username"])
+            users_to_check.append(user["username"])
 
         return users_to_check
 
@@ -326,11 +330,10 @@ class CreateChat(QWidget):
 
         if username in self.users:
             requests.post(
-                BASE_URL + 'api/chats/',
-                json={'sent_from_id': self.current_user,
-                      'sent_to_id': username},
-                headers={'Authorization': f'Token {self.token}'}
-                )
+                BASE_URL + "api/chats/",
+                json={"sent_from_id": self.current_user, "sent_to_id": username},
+                headers={"Authorization": f"Token {self.token}"},
+            )
             self.availableChats = AvailableChats(self.token, self.current_user)
 
             self.hide()
@@ -340,7 +343,6 @@ class CreateChat(QWidget):
 
 
 class Chat(QMainWindow):
-
     def __init__(self, chat_num, token):
         super().__init__()
         self.chat_num = chat_num
@@ -349,7 +351,7 @@ class Chat(QMainWindow):
 
         self.resize(400, 400)
         self.center()
-        self.setWindowTitle('Chat')
+        self.setWindowTitle("Chat")
         self.show()
 
     def center(self):
@@ -368,19 +370,18 @@ class Chat(QMainWindow):
     def createMenu(self):
         menuBar = self.menuBar()
 
-        fileMenu = menuBar.addMenu('&File')
+        fileMenu = menuBar.addMenu("&File")
         fileMenu.addAction(self.createExitAction())
 
-    def createExitAction(self):   # TODO: для дублирующихся функций сделай Mix-in
-        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
+    def createExitAction(self):  # TODO: для дублирующихся функций сделай Mix-in
+        exitAction = QAction(QIcon("exit.png"), "&Exit", self)
+        exitAction.setShortcut("Ctrl+Q")
+        exitAction.setStatusTip("Exit application")
         exitAction.triggered.connect(self.close)
         return exitAction
 
 
 class CentralWidget(QWidget):
-
     def __init__(self, chat_num, token):
         super().__init__()
         self.chat_num = chat_num
@@ -392,7 +393,7 @@ class CentralWidget(QWidget):
         self.textbox.setReadOnly(True)
 
         self.chat = QLineEdit(self)
-        self.sendBtn = QPushButton('Send', self)
+        self.send_button = QPushButton("Send", self)
 
         self.messages = ChatThread(self.chat_num)
         self.messages.daemon = True
@@ -402,14 +403,14 @@ class CentralWidget(QWidget):
 
         self.get_chat_messages()
 
-        self.sendBtn.clicked.connect(self.send_message)
+        self.send_button.clicked.connect(self.send_message)
         self.messages.rcv.connect(self.receive_message)
 
         grid = QGridLayout()
         grid.setSpacing(3)
         grid.addWidget(self.textbox, 0, 0, 1, 3)
         grid.addWidget(self.chat, 1, 0, 1, 1)
-        grid.addWidget(self.sendBtn, 1, 2)
+        grid.addWidget(self.send_button, 1, 2)
 
         self.setLayout(grid)
 
@@ -427,8 +428,8 @@ class CentralWidget(QWidget):
     @pyqtSlot()
     def get_chat_messages(self):
         chat_history = requests.get(
-            BASE_URL + f'api/chat/{self.chat_num}/',
-            headers={'Authorization': f'Token {self.token}'}
+            BASE_URL + f"api/chat/{self.chat_num}/",
+            headers={"Authorization": f"Token {self.token}"},
         ).json()
 
         self.offset = calculate_offset(self.chat_num, self.token)
@@ -436,14 +437,14 @@ class CentralWidget(QWidget):
         current_offset = 0
         for _, message in enumerate(chat_history):
 
-            message['message'] = json.loads(message['message'])
-            message_len = len(message['message'])
+            message["message"] = json.loads(message["message"])
+            message_len = len(message["message"])
 
-            key = generate_key(message['message'], current_offset)
+            key = generate_key(message["message"], current_offset)
 
             current_offset += message_len
 
-            decrypted_message = decrypt(message['message'], key)
+            decrypted_message = decrypt(message["message"], key)
 
             self.textbox.appendPlainText(decrypted_message)
 
@@ -454,12 +455,12 @@ class CentralWidget(QWidget):
         encrypted_message = encrypt(send_value, self.key)
 
         if send_value:
-            send_value = json.dumps({'message': encrypted_message})
+            send_value = json.dumps({"message": encrypted_message})
             self.messages.send_wrapper(send_value)
-            self.chat.setText('')
+            self.chat.setText("")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     message_queue = queue.Queue()  # TODO: Лучше передавать по ссылке в классы
     app = QApplication(sys.argv)
     chat = Menu()
